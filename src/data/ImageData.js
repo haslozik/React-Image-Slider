4e127d2bb0b7473eba90287fe560472274a35dec
haslozik <wojciechperlinskitpel@gmail.com>
2021-08-22 19:27:58 +0200

    images Data added / component added / function to finish

---
+++ src/data/ImageData.js
@@ -0,0 +1,27 @@
+import Img1 from '../Images/img1.jpg';
+import Img2 from '../Images/img2.jpg';
+import Img3 from '../Images/img3.jpg';
+import Img4 from '../Images/img4.jpg';
+import Img5 from '../Images/img5.jpg';
+import Img6 from '../Images/img6.jpg';
+
+export const SliderData = [
+    {
+        image: Img1
+    },
+    {
+        image: Img2
+    },
+    {
+        image: Img3
+    },
+    {
+        image: Img4
+    },
+    {
+        image: Img5
+    },
+    {
+        image: Img6
+    }
+]
